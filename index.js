9b370de96bd72dda9f61194865ee61bf33a06887
WildCodeSchool <anelisynagano@gmail.com>
2019-12-13 15:34:29 +0000

    add collapsible div

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const textureBackground = document.getElementsByTagName('body');
-console.log(textureBackground);
+const textureBackground = document.getElementsByTagName('body')[0];
 const textureSelection = document.getElementById("texture-selector");
+const collapse = document.getElementsByClassName("collapse")[0];
+const title = document.getElementsByClassName("title")[0];
 
-textureSelection.addEventListener('change', () => textureBackground[0].style.backgroundImage = `url(${event.target.value})`);
+collapse.style.display = 'none';
+
+textureSelection.addEventListener('change', () => textureBackground.style.backgroundImage = `url(${event.target.value})`);
+
+title.addEventListener('click', () => {
+    if (collapse.style.display === 'none' ) {
+        collapse.style.display = 'block'
+    } else {
+        collapse.style.display = 'none'
+    }
+});
